11e6fdeded5f381385a21388f8d8be2131a2930d
06_Mai Chi_0049 <148560917+MaiChi26@users.noreply.github.com>
2024-03-13 22:42:26 +0700

    btvn

---
+++ Lab1/bai2.py
@@ -0,0 +1,9 @@
+
+#bài2:
+so_luong_sach = input("nhập ngày sinh của sinh viên: ") 
+ten_sach = input("nhập tên sách: ") 
+ma_sach = input("nhập mã sinh viên: ")
+tac_gia = input("nhập tên tác giả theo sách đã chọn: ")   
+nam_xuat_ban = input("nhập năm xuất bản theo sách đã chọn: ")
+print("Thư viện DHKTKTCN có {} sách {} với mã số {}.Cuốn sách của tác giả {} được xuất bản vào năm {}.".format(so_luong_sach,ten_sach,ma_sach,tac_gia,nam_xuat_ban))
+
